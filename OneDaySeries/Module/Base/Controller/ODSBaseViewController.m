689dccb1e968f62f573c047327b87bb61e1d1b8a
Tolecen <taox.l@163.com>
2016-07-01 16:38:58 +0800

    setv

--- OneDaySeries/Module/Base/Controller/ODSBaseViewController.m
+++ OneDaySeries/Module/Base/Controller/ODSBaseViewController.m
@@ -13,16 +13,21 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
-    self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"chaifei_nav_title"]];
+    
+    if ([self isEqual:self.navigationController.viewControllers[0]]) {
+        self.navigationItem.titleView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"chaifei_nav_title"]];
+    }
+    
     
     if (self.navigationController.viewControllers.count >= 2) {
-        UIBarButtonItem *leftButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"返回"
+        UIBarButtonItem *leftButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"ㄑ 返回"
                                                                            style:UIBarButtonItemStyleDone
                                                                           target:self
                                                                           action:@selector(inner_Pop:)];
         self.navigationItem.leftBarButtonItem = leftButtonItem;
-        [self.navigationItem.leftBarButtonItem setTitleTextAttributes:@{NSFontAttributeName: [UIFont boldSystemFontOfSize:13.f], NSForegroundColorAttributeName: [UIColor colorWithRGB:0x03225C]} forState:UIControlStateNormal];
+        [self.navigationItem.leftBarButtonItem setTitleTextAttributes:@{NSFontAttributeName: [UIFont boldSystemFontOfSize:15.f], NSForegroundColorAttributeName: [UIColor colorWithRGB:0x03225C]} forState:UIControlStateNormal];
         [self.navigationItem.leftBarButtonItem setTitlePositionAdjustment:UIOffsetMake(10, 0) forBarMetrics:UIBarMetricsDefault];
+        self.navigationController.interactivePopGestureRecognizer.delegate = self;
     }
 }
 
