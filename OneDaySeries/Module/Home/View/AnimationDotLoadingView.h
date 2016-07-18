d40e3a747c165e5950a6d953226a34019c4c1e69
Tolecen <taox.l@163.com>
2016-07-18 11:30:37 +0800

    dot loading

---
+++ OneDaySeries/Module/Home/View/AnimationDotLoadingView.h
@@ -0,0 +1,14 @@
+//
+//  AnimationDotLoadingView.h
+//  OneDaySeries
+//
+//  Created by TaoXinle on 16/7/18.
+//  Copyright © 2016年 cn.com.uzero. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AnimationDotLoadingView : UIView
+-(void)showInView:(UIView *)view;
+-(void)dismiss;
+@end
